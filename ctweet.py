3cd3f71ecffe57543090f47a963ab3271aa12771
Dan Loewenherz <dloewenherz@gmail.com>
2010-07-28 15:55:31 -0700

    commit growl notifications

--- ctweet.py
+++ ctweet.py
@@ -7,11 +7,10 @@ import json
 import bsddb
 import urllib2
 import getpass
+import Growl as growl
 
-GREEN = "\033[01;32m"
-RED = "\033[01;31m"
-WHITE = "\033[00;37m"
-AQUA = "\033[01;36m"
+notifier = growl.GrowlNotifier("Twitter Update", [""])
+notifier.register()
 
 def tweet_info(tweet_id = 1234):
     url = "http://api.twitter.com/1/statuses/show.json?id=%d" % tweet_id
@@ -24,16 +23,15 @@ def user_info(user_id = 1234):
     return response.read()
 
 class http_client(asyncore.dispatcher):
-
     def __init__(self, host, path, username, password):
         authorization = "%s:%s" % (username, password)
         authorization = authorization.encode("base-64")[:-1]
 
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect( (host, 80) )
-        self.buffer = 'GET %s HTTP/1.0\r\nAuthorization: Basic %s\r\n\r\n' % (path, authorization)
-        self.tweet_buffer = ""
+        self.connect((host, 80))
+        self.send_buffer = 'GET %s HTTP/1.0\r\nAuthorization: Basic %s\r\n\r\n' % (path, authorization)
+        self.receive_send_buffer = ""
         self.db = bsddb.btopen("/tmp/twitter.db", 'c')
 
     def handle_connect(self):
@@ -43,77 +41,82 @@ class http_client(asyncore.dispatcher):
         self.close()
 
     def handle_read(self):
-        d = self.recv(8192)
-        self.tweet_buffer += d.strip()
+        data = self.recv(8192)
+        self.receive_send_buffer += data
 
-        try:
-            for item in self.tweet_buffer.split("\r\n"):
-                lb_count = self.tweet_buffer.count('{')
-                rb_count = self.tweet_buffer.count('}')
+        if self.receive_send_buffer == "":
+            return
 
-                if lb_count == rb_count and len(self.tweet_buffer) > 0:
-                    left_brace = self.tweet_buffer.index('{')
-                    right_brace = self.tweet_buffer.rindex('}')
-                    data = json.loads(self.tweet_buffer[left_brace:right_brace+1])
+        if self.receive_send_buffer.endswith("\r\n"):
+            try:
+                data = json.loads(self.receive_send_buffer)
+            except:
+                print "EXCEPTION: ", len(self.receive_send_buffer), " ", self.receive_send_buffer
+                data = {}
 
-                    if 'delete' in data:
-                        key = 'user_%s' % data['delete']['status']['user_id']
-                        source = self.db.get(key)
-                        if not source:
-                            source = user_info(data['delete']['status']['user_id'])
-                            self.db[key] = source
-                        source_data = json.loads(source)
+            if 'delete' in data:
+                key = 'user_%s' % data['delete']['status']['user_id']
+                source = self.db.get(key)
+                if not source:
+                    source = user_info(data['delete']['status']['user_id'])
+                    self.db[key] = source
+                source_data = json.loads(source)
 
-                        print RED + "<deletion>" + WHITE + \
-                                " %s deleted a tweet (%d)" % (source_data['screen_name'], data['delete']['status']['id'])
-                    elif 'event' in data:
+                image_data = urllib2.urlopen(source_data['profile_image_url']).read()
+                icon = growl.Image.imageWithData(image_data)
+                notifier.notify(noteType = "", title = source_data['screen_name'], description = \
+                  "%s deleted a tweet (%d)" % (source_data['screen_name'], \
+                  data['delete']['status']['id']), icon = icon)
 
-                        key = 'user_%s' % data['source']['id']
-                        source = self.db.get(key)
-                        if not source:
-                            source = user_info(data['source']['id'])
-                            self.db[key] = source
-                        source_data = json.loads(source)
+            elif 'event' in data:
+                key = 'user_%s' % data['source']['id']
+                source = self.db.get(key)
+                if not source:
+                    source = user_info(data['source']['id'])
+                    self.db[key] = source
+                source_data = json.loads(source)
 
-                        key = 'user_%s' % data['target']['id']
-                        target = self.db.get(key)
-                        if not target:
-                            target = user_info(data['target']['id'])
-                            self.db[key] = target
-                        target_data = json.loads(target)
+                key = 'user_%s' % data['target']['id']
+                target = self.db.get(key)
+                if not target:
+                    target = user_info(data['target']['id'])
+                    self.db[key] = target
+                target_data = json.loads(target)
 
-                        if data['event'] in ['favorite', 'unfavorite', 'retweet']:
-                            key = 'tweet_%s' % data['target_object']['id']
-                            tweet = self.db.get(key)
-                            if not tweet:
-                                tweet = tweet_info(data['target_object']['id'])
-                                self.db[key] = tweet
-                            tweet_data = json.loads(tweet)
+                if data['event'] in ['favorite', 'unfavorite', 'retweet']:
+                    key = 'tweet_%s' % data['target_object']['id']
+                    tweet = self.db.get(key)
+                    if not tweet:
+                        tweet = tweet_info(data['target_object']['id'])
+                        self.db[key] = tweet
+                    tweet_data = json.loads(tweet)
 
-                            print GREEN + "<%s> " % data['event'] + WHITE + \
-                                    "by %s, %s: %s" % (source_data['screen_name'], target_data['screen_name'], tweet_data['text'])
-                        elif data['event'] == 'follow':
-                            print GREEN + "<follow> " + WHITE + "%s started following %s" % (source_data['screen_name'],
-                                    target_data['screen_name'])
-                        else:
-                            print GREEN + "<%s> " % data['event'] + WHITE + \
-                                    "source: %s, target: %s" % (source_data['screen_name'], target_data['screen_name'])
-                    elif 'id' in data:
-                        print AQUA + "<tweet> " + WHITE + \
-                                data['user']['screen_name'] + ": " + data['text']
+                    notifier.notify(noteType = "", title = data['event'], description = \
+                        "%s by %s, %s: %s" % (data['event'], source_data['screen_name'], target_data['screen_name'],
+                            tweet_data['text']))
+                elif data['event'] == 'follow':
+                    notifier.notify(noteType = "", title = 'follow', description = \
+                        "%s started following %s" % (source_data['screen_name'], target_data['screen_name']))
+                else:
+                    notifier.notify(noteType = "", title = 'unknown', description = \
+                        "source: %s, target: %s" % (source_data['screen_name'], target_data['screen_name']))
 
-                    self.tweet_buffer = ""
-        except Exception, e:
-            pass
+            elif 'id' in data:
+                image_data = urllib2.urlopen(data['user']['profile_image_url']).read()
+                icon = growl.Image.imageWithData(image_data)
+                notifier.notify(noteType = "", title = data['user']['screen_name'], description = \
+                  data['text'], icon = icon)
+
+            self.receive_send_buffer = ""
 
         self.db.sync()
 
     def writable(self):
-        return (len(self.buffer) > 0)
+        return (len(self.send_buffer) > 0)
 
     def handle_write(self):
-        sent = self.send(self.buffer)
-        self.buffer = self.buffer[sent:]
+        sent = self.send(self.send_buffer)
+        self.send_buffer = self.send_buffer[sent:]
 
 if __name__ == '__main__':
     username = sys.argv[1]
